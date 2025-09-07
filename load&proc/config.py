cbfe9deb5d9e9675775335435453002a24ba040a
itai <itaifuchs@gmail.com>
2025-09-07 14:56:15 +0300

    create loder files path

---
+++ load&proc/config.py
@@ -0,0 +1,13 @@
+import logging
+
+
+#log config
+logging.basicConfig(level=logging.WARNING)
+
+
+file_path="C:/podcasts/download (3).wav"
+
+
+KAFKA_BROKERS = ["localhost:9092"]
+
+topic="PODCASTS_INFO"
---
+++ load&proc/loader.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+import logging
+logger = logging.getLogger(__name__)
+
+class DirectoryScanner:
+    def __init__(self, directory):
+        try:
+            self.directory = Path(directory)
+        except FileNotFoundError:
+            logger.error(f"Error: { self.directory} not found. Please ensure the file exists.")
+
+    def get_files(self):
+
+            return [f for f in self.directory.iterdir() if f.is_file()]
+
+
+
+
